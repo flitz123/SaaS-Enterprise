f4be56ee48625c813355fc237799b8ded327b8a1
flitz123 <127119426+flitz123@users.noreply.github.com>
2026-09-25 15:35:14 +0400

    update vite .env

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_URL?: string;
+  readonly VITE_API_URL?: "https://saas-enterprise-2ehf.onrender.com/";
 }
 
 interface ImportMeta {
